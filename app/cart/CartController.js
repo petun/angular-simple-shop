79debe7b0bd6d80e6c1ec6a0e3929d9b595fafb2
petun <petun911@gmail.com>
2016-06-17 15:32:53 +0300

    'init';

---
+++ app/cart/CartController.js
@@ -0,0 +1,10 @@
+angular.module('CartModule', []).controller('CartController', function($log, $scope) {
+    $scope.getTotal = function () {
+        return 0;
+    };
+
+
+    $scope.getCost = function () {
+        return 0;
+    }
+});
