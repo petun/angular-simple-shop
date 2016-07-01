16dc9107f85955817f4f966e7a5499514544620e
petun <petun911@gmail.com>
2016-07-01 09:49:21 +0300

    add to cart - add message (add to cart)

--- app/cart/directives/addToCart.js
+++ app/cart/directives/addToCart.js
@@ -6,14 +6,25 @@ angular.module('CartModule')
             product: '@'
         },
         template: '<a href="" ng-click="addToCart()">Add to cart</a>',
-        controller: function($log, $scope, dataServiceCart) {
+        controller: function($log, $scope, dataServiceCart, $timeout) {
             $scope.product = JSON.parse($scope.product);
 
             $scope.addToCart = function() {
                 $log.log($scope.product);
                 var product = JSON.parse($scope.product);
                 dataServiceCart.cart.addItem(product.id, product.name, product.price, 1);
+
+                var element = angular.element('<div>Successfully add to cart</div>');
+                $scope.element.append(element);
+
+                $timeout(function(){
+                    element.remove();
+                }, 1000);
             }
+        },
+
+        link: function (scope, element, attr) {
+            scope.element = angular.element(element);
         }
     };
 });
